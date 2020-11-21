b37088a989f3e9ccf62a1e5391af2359ac37b900
jaydenbf <47370644+jaydenbf@users.noreply.github.com>
2020-11-21 15:10:22 -0800

    Defenses & Shop
    Added less temporary (but still temporary) defense sprites, added highlighting to tiles (one cast still doesn't work), fixed defense sprite loading

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,9 +17,14 @@ public class GameManager : MonoBehaviour
 
     public static PurchaseState purchaseState;
     public static Tilemap groundTiles;
-    public static ArrayList reservedTiles;
+    private static ArrayList reservedTiles;
+    private static int trueReservedTiles;
+    private static Vector3Int curTile;
 
     private static GameObject[] defenses;
+    private static Tile canPlace;
+    private static Tile cannotPlace;
+    private static Tile background;
 
     // Start is called before the first frame update
     void Start()
@@ -37,14 +42,22 @@ public class GameManager : MonoBehaviour
                 new Vector3Int(int.Parse(coords[0], System.Globalization.NumberStyles.AllowLeadingSign), 
                 int.Parse(coords[1], System.Globalization.NumberStyles.AllowLeadingSign), 0));
         }
+        trueReservedTiles = lines.Length;
 
         defenses = new GameObject[4];
-        defenses[0] = (GameObject)Resources.Load("Prefabs/Defense1");
-        defenses[1] = (GameObject)Resources.Load("Prefabs/Defense2");
-        defenses[2] = (GameObject)Resources.Load("Prefabs/Defense3");
-        defenses[3] = (GameObject)Resources.Load("Prefabs/Defense4");
+        defenses[0] = (GameObject)Resources.Load("Prefabs/DefenseObjects/Defense1");
+        defenses[1] = (GameObject)Resources.Load("Prefabs/DefenseObjects/Defense2");
+        defenses[2] = (GameObject)Resources.Load("Prefabs/DefenseObjects/Defense3");
+        defenses[3] = (GameObject)Resources.Load("Prefabs/DefenseObjects/Defense4");
+        background = (Tile)Resources.Load("Tilesets/Tiles/Ground Tiles/TileSet_V2_47");
+        canPlace = (Tile)Resources.Load("Tilesets/Tiles/Ground Tiles/canPlace");
+        cannotPlace = (Tile)Resources.Load("Tilesets/Tiles/Ground Tiles/cannotPlace");
+        curTile = new Vector3Int(int.MaxValue, int.MaxValue, int.MaxValue);
 
         Debug.Assert(groundTiles != null);
+        Debug.Assert(canPlace != null);
+        Debug.Assert(cannotPlace != null);
+        Debug.Assert(background != null);
     }
 
     private void OnGUI()
@@ -73,55 +86,88 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+        Vector3Int tilePos = groundTiles.WorldToCell(worldPoint);
+
+        UpdateCursorTile(tilePos);
+
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
-            Vector3Int tilePos = groundTiles.WorldToCell(worldPoint);
-
             Debug.Log(tilePos.x + " " + tilePos.y);
             if (reservedTiles.Contains(tilePos))
             {
                 Debug.Log("This tile is reserved.");
-                purchaseState = PurchaseState.None;
+                //purchaseState = PurchaseState.None;
             }
             else if (purchaseState != PurchaseState.None)
             {
-                switch (purchaseState)
-                {
-                    case PurchaseState.Defense1:
-                        Instantiate(defenses[0],
-                            new Vector3(groundTiles.GetCellCenterWorld(tilePos).x,
-                            groundTiles.GetCellCenterWorld(tilePos).y, 0),
-                            Quaternion.identity);
-                        Debug.Log("Placed defense 1");
-                        break;
-                    case PurchaseState.Defense2:
-                        Instantiate(defenses[1],
-                            new Vector3(groundTiles.GetCellCenterWorld(tilePos).x,
-                            groundTiles.GetCellCenterWorld(tilePos).y, 0),
-                            Quaternion.identity);
-                        Debug.Log("Placed defense 2");
-                        break;
-                    case PurchaseState.Defense3:
-                        Instantiate(defenses[2],
-                            new Vector3(groundTiles.GetCellCenterWorld(tilePos).x,
-                            groundTiles.GetCellCenterWorld(tilePos).y, 0),
-                            Quaternion.identity);
-                        Debug.Log("Placed defense 3");
-                        break;
-                    case PurchaseState.Defense4:
-                        Instantiate(defenses[3],
-                            new Vector3(groundTiles.GetCellCenterWorld(tilePos).x,
-                            groundTiles.GetCellCenterWorld(tilePos).y, 0),
-                            Quaternion.identity);
-                        Debug.Log("Placed defense 4");
-                        break;
-                    default:
-                        break;
-                }
-                reservedTiles.Add(tilePos);
+                PlaceDefense(tilePos);
             }
         }
+    }
+
+    private void UpdateCursorTile(Vector3Int tilePos)
+    {
+        // if it's a true reserved tile
+        if (isTrueReservedTile(tilePos))
+        {
+            groundTiles.SetTile(curTile, background);
+            return;
+        }
+        groundTiles.SetTile(curTile, background);
+        if (reservedTiles.Contains(tilePos)) // regular reserved tile
+            groundTiles.SetTile(tilePos, cannotPlace);
+        else // free real estate
+            groundTiles.SetTile(tilePos, canPlace);
+            curTile = tilePos;
+    }
+
+    private bool isTrueReservedTile(Vector3Int tilePos)
+    {
+        for (int i = 0; i < trueReservedTiles; i++)
+        {
+            if ((Vector3Int)reservedTiles[i] == tilePos)
+                return true;
+        }
+        return false;
+    }
+
+    private void PlaceDefense(Vector3Int tilePos)
+    {
+        switch (purchaseState)
+        {
+            case PurchaseState.Defense1:
+                Instantiate(defenses[0],
+                    new Vector3(groundTiles.GetCellCenterWorld(tilePos).x,
+                    groundTiles.GetCellCenterWorld(tilePos).y, 0),
+                    Quaternion.identity);
+                Debug.Log("Placed defense 1");
+                break;
+            case PurchaseState.Defense2:
+                Instantiate(defenses[1],
+                    new Vector3(groundTiles.GetCellCenterWorld(tilePos).x,
+                    groundTiles.GetCellCenterWorld(tilePos).y, 0),
+                    Quaternion.identity);
+                Debug.Log("Placed defense 2");
+                break;
+            case PurchaseState.Defense3:
+                Instantiate(defenses[2],
+                    new Vector3(groundTiles.GetCellCenterWorld(tilePos).x,
+                    groundTiles.GetCellCenterWorld(tilePos).y, 0),
+                    Quaternion.identity);
+                Debug.Log("Placed defense 3");
+                break;
+            case PurchaseState.Defense4:
+                Instantiate(defenses[3],
+                    new Vector3(groundTiles.GetCellCenterWorld(tilePos).x,
+                    groundTiles.GetCellCenterWorld(tilePos).y, 0),
+                    Quaternion.identity);
+                Debug.Log("Placed defense 4");
+                break;
+            default:
+                break;
+        }
+        reservedTiles.Add(tilePos);
     }
 }
