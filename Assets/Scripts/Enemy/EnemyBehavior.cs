ffb330366d2567b6db76666b9375866b2c8e31c3
vshaw18 <vshaw18@uw.edu>
2020-11-20 17:44:23 -0800

    Uploaded Temp Enemy Objects and reoragnize the scripts.
    -Working on the spawn system later tonight and tomorrow morning

---
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public bool alive = true;
+    public int health = 100;
+
+    void Update()
+    {
+        IsAlive();
+    }
+
+    private void IsAlive()
+    {
+        if (health <= 0)
+        {
+            // Change later where we deestroy enenmy as walking out. 
+            alive = false;
+        }
+
+        if (alive)
+        {
+            //Destroy(transform.parent.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void AttackDamageRecieved(int damage)
+    {
+        health -= damage;
+    }
+
+    // Probably have to add a collider
+
+
+}
