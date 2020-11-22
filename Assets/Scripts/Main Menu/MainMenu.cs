1bbed8abf3b518dc0b76a1d5e386ed862042437e
vshaw18 <vshaw18@uw.edu>
2020-11-22 12:02:00 -0800

    Main Menu Sceen
    - Added the framework for the main menu from shape defender.
    - Can add more to the background
    - Change font later

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
