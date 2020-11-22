bd5540fa785dfd5e995393f8e8ede8768a56fe82
Kyle Dukart <46544456+kyledukart@users.noreply.github.com>
2020-11-21 16:02:38 -0800

    add first tower and projectile

---
+++ Assets/Scripts/Towers/Projectile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    private Transform target;
+
+    public float speed = 70f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if projectile has no target it is destroyed
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        // object hit
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    // set the target for the projectile
+    public void setTarget(Transform target_in)
+    {
+        target = target_in;
+    }
+
+    void HitTarget()
+    {
+        // lower enemy hp
+        // play hit effect?
+        Destroy(gameObject);
+    }
+}
