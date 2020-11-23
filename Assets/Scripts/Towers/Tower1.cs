7b4282e8efb3158885e94333e4e8b3c89c42a57a
Kyle Dukart <46544456+kyledukart@users.noreply.github.com>
2020-11-22 23:22:24 -0800

    bug fix

--- Assets/Scripts/Towers/Tower1.cs
+++ Assets/Scripts/Towers/Tower1.cs
@@ -96,7 +96,7 @@ public class Tower1 : MonoBehaviour
         if (bullet != null)
         {
             bullet.setTarget(target);
-            bullet.SetDamage(damage);
+            bullet.setDamage(damage);
         }
     }
 
