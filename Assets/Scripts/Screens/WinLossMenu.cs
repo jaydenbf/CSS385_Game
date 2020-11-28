04fc86a1b606bb99eeaeffa114add3e907ee8360
vshaw18 <vshaw18@uw.edu>
2020-11-28 11:40:46 -0800

    Updated Win/Loss Screen
    - Showed various game statistics (round reached, lives left, enemy killed counts)
    - Bug: Does not reflect true kill count in game. Will need to be fix later

--- Assets/Scripts/Screens/WinLossMenu.cs
+++ Assets/Scripts/Screens/WinLossMenu.cs
@@ -7,10 +7,20 @@ using UnityEngine.SceneManagement;
 public class WinLossMenu : MonoBehaviour
 {
     public Text roundReachUI;
-    public Text livesLeftUI; 
+    public Text livesLeftUI;
+    public Text totalEnemyKilled;
+    public Text flyingEyeCount;
+    public Text goblinCount;
+    public Text mushroomCount;
+    public Text skeletonCount;
 
     private static int roundReach;
     private static int livesLeft;
+
+    private static int flyingEyeKilled;
+    private static int goblinKilled;
+    private static int mushroomKilled;
+    private static int skeletonKilled;
 
     void Update()
     {
@@ -19,17 +29,34 @@ public class WinLossMenu : MonoBehaviour
 
     private void ShowText()
     {
+        // Get Various Game Statistics from game manager
         roundReach = GameManager.lastRound;
         livesLeft = GameManager.lastLives;
+        flyingEyeKilled = GameManager.lastKilledFlyingEye;
+        goblinKilled = GameManager.lastKilledGoblin;
+        mushroomKilled = GameManager.lastKilledMushroom;
+        skeletonKilled = GameManager.lastKilledSkeleton;
 
-        roundReachUI.text = "Round Reached:\t\t" + roundReach + " / 7";
+        roundReachUI.text = "Round Reached:\t\t\t\t\t" + roundReach + " / 7";
 
+        // Show Lives Count
         if(livesLeft < 0)
         {
             livesLeft = 0;
         }
 
-        livesLeftUI.text = "Lives Left:\t\t" + livesLeft + " / 100";
+        livesLeftUI.text = "Lives Left:\t\t\t\t\t\t\t\t" + livesLeft + " / 100";
+
+        int totalAmount = flyingEyeKilled + goblinKilled + mushroomKilled + skeletonKilled;
+        // Show total Enemy kill count
+        totalEnemyKilled.text = "Total Enemies Killed:\t\t\t" + totalAmount;
+
+        // Show Individual Enemy Kill Count
+        flyingEyeCount.text = "" + flyingEyeKilled;
+        goblinCount.text = "" + goblinKilled;
+        mushroomCount.text = "" + mushroomKilled;
+        skeletonCount.text = "" + skeletonKilled;
+
     }
 
     public void LoadMainMenu()
