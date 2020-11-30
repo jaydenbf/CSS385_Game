457f8be3d894ba82903f85b878a42d9abdd82307
jaydenbf <47370644+jaydenbf@users.noreply.github.com>
2020-11-29 20:10:50 -0800

    New Branch

--- Assets/Scripts/Towers/TowerSelect.cs
+++ Assets/Scripts/Towers/TowerSelect.cs
@@ -27,31 +27,7 @@ public class TowerSelect : MonoBehaviour
 
     public void SetTarget(Tower1 tower_in)
     {
-        if(tower == null)
-        {
-            tower = tower_in;
-            tower.isSelected = true;
-        } else
-        {
-            tower.isSelected = false;
-            tower = tower_in;
-            tower.isSelected = true;
-        }
-
-        //if(tower == null)
-        //{
-        //    Debug.Log("NEW TOWER SELECTED");
-        //    tower.isSelected = false;
-        //    tower = tower_in;
-        //    tower.isSelected = true;
-        //} else
-        //{
-        //    Debug.Log("OLD TOWER UNSELECTED");
-        //    tower.isSelected = false;
-        //    tower = tower_in;
-        //    tower.isSelected = true;
-        //    Debug.Log("NEW TOWER SELECTED");
-        //}
+        tower = tower_in;
 
         transform.position = tower.transform.position;
 
@@ -60,56 +36,41 @@ public class TowerSelect : MonoBehaviour
         upgradeButtonText.text = tower.getUpgradeInfo();
 
         //set range indicator size... this is not exact
-        //rangeIndicator.transform.localScale = new Vector2(tower.range * .8f, tower.range * .8f);
-        //rangeIndicator.enabled = true;
-        //ui.SetActive(true);
+        rangeIndicator.transform.localScale = new Vector2(tower.range * .8f, tower.range * .8f);
+        rangeIndicator.enabled = true;
+        ui.SetActive(true);
     }
 
     public void Hide()
     {
-        if(tower != null)
-        {
-            tower.isSelected = false;
-            tower = null;
-            //towerDetails.text = "";
-            //sellButtonText.text = "";
-            //upgradeButtonText.text = "";
-        }
-
-        //rangeIndicator.enabled = false;
-        //ui.SetActive(false);
+        rangeIndicator.enabled = false;
+        ui.SetActive(false);
     }
 
     public void ClickToSell()
     {
         // refund cash
-            gmanager.addCash(tower.cost);
-            tower.cost = 0;
-            tower.Destroy();
+        GameManager.addCash(tower.cost);
+        tower.cost = 0;
+        tower.Destroy();
 
-            Hide();
+        Hide();
     }
 
     public void ClickToUpgrade()
     {
-        if (gmanager.GetCash() > tower.towerUpgrade.cost)
+        // this check is not working
+        if ((GameManager.GetCash() > tower.towerUpgrade.cost) && (tower.towerUpgrade != null) && tower.level < 3)
         {
+            Debug.Log("Cash: " + GameManager.GetCash());
             // remove cash
-            gmanager.RemoveCash(tower.towerUpgrade.cost);
+            GameManager.RemoveCash(tower.towerUpgrade.cost);
+            
+            tower.Upgrade();
 
-            // instantiate new tower
-            Tower1 newTowerInfo = tower.towerUpgrade;
-            Vector3 newTowerPosition = tower.transform.position;
-            Quaternion newTowerRotation = tower.transform.rotation;
-
-            // destroy old tower
-            tower.Destroy();
-
-            // create new tower
-            Tower1 newTower = Instantiate(newTowerInfo, newTowerPosition, newTowerRotation);
-
-            tower = newTower;
-
+            sellButtonText.text = tower.getSellInfo();
+            upgradeButtonText.text = tower.getUpgradeInfo();
+            SetTarget(tower);
         }
     }
 
