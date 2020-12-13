eeda81a37788a1c42e0c72a6c9d74751cc8f4246
vshaw18 <vshaw18@uw.edu>
2020-12-13 12:39:56 -0800

    - Music Volume Consistent across the scenes
    - Sliders are not sync across scenes

--- Assets/Scripts/Music/DontDestory.cs
+++ Assets/Scripts/Music/DontDestory.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DontDestory : MonoBehaviour
 {
+    public static float musicVol;
+    public static float sfxVol;
+
+    private int sceneID;
+    private bool updateSliderOnce = false;
     void Awake()
     {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
@@ -14,5 +21,24 @@ public class DontDestory : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
+
+        sceneID = SceneManager.GetActiveScene().buildIndex;
+        updateSliderOnce = false;
+        musicVol = 0.5f;
+        sfxVol = 0.5f;
+    }
+
+    void Update()
+    {
+        if(sceneID != SceneManager.GetActiveScene().buildIndex)
+        {
+            updateSliderOnce = true;
+            sceneID = SceneManager.GetActiveScene().buildIndex;
+        }
+
+        if (updateSliderOnce)
+        {
+            // Change slider component to match 
+        }
     }
 }
---
+++ Assets/Scripts/Music/SetVolume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public AudioMixer sfx;
+
+    public void SetLevel(float slideValue)
+    {
+        // Call dont destroy on load to save values
+        DontDestory.musicVol = slideValue;
+        mixer.SetFloat("MusicVolume", Mathf.Log10(slideValue) * 20f);
+    }
+
+    public void SetLevelSFX(float slideValue)
+    {
+        // sfx.SetFloat("Exposed param", Mathf.Log10(slideValue) * 20f);
+    }
+}
