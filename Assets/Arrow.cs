799451d779279bc9b7436c025c0e9f6ca9841c57
SammyCLew <SammyCLew@icloud.com>
2020-12-13 17:13:49 -0800

    Arrows

---
+++ Assets/Arrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public EnemySpawnSystem e;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(e.spawnWave)
+        {
+            SpriteRenderer r = GetComponent<SpriteRenderer>();
+            r.color = new Color(0f, 0f, 0f, 0f);
+        } else
+        {
+            SpriteRenderer r = GetComponent<SpriteRenderer>();
+            r.color = new Color(1f, 1f, 1f, 1f);
+        }
+    }
+}
